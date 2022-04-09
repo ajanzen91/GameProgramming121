1bb17dc63fac7ef3690fb01082b96adaceb36a6e
SPACEMAN0644 <36048883+SPACEMAN0644@users.noreply.github.com>
2022-04-08 19:01:58 -0500

    Initial test room (incomplete, issues, debugs)
    Issues
    - Brute enemy's script no longer works; haven't been able to fix it
    
    Important changes
    - Started test room
    - Using ProBuilder now
    - Changed assets for easier prototyping
    - For this branch, removed projects I did not modify/work on
    
    Misc
    - Changed debug log statements as i am working on the jumping bug

---
+++ M.E.S.S.S/Assets/Scripts/Brute.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brute : MonoBehaviour
+{
+    //Rotation variables
+    private float _rotationRate;
+    public int _rotationSpeed = 10;
+    public int _rotationCooldown = 15;
+    private int _currentCooldown;
+    private Quaternion _newRotation;
+    private bool _rotatingFlag;
+
+    //planar movement variables
+    private float _moveSpeed;
+    public float _walkSpeed = 1f;
+    public float _runSpeed = 4f;
+    private Rigidbody _rigidbody;
+
+    //ray variables
+    private Ray _ray;
+    private RaycastHit _seenPlayer;
+
+    //Enemy stats
+    public int _damagePerHit = 20;
+    public int _attackCoolDown = 5;
+    public float _sightRange = 30f;
+    private bool _chargingFlag;
+    public int _hp = 100;
+
+    void Start()
+    {
+        _rigidbody = gameObject.GetComponent<Rigidbody>();
+        _ray = new Ray(transform.position, transform.forward);
+        _moveSpeed = _walkSpeed;
+        _currentCooldown = _rotationCooldown;
+        _chargingFlag = false;
+        _rotatingFlag = false;
+    }
+
+    void Update()
+    {
+        _chargingFlag = Physics.Raycast(transform.position, transform.forward, out _seenPlayer, Mathf.Infinity);
+        //Debug.Log(_chargingFlag);
+
+        if (_chargingFlag)
+        {
+            Charge();
+        }
+        else
+        {
+            Wander();
+        }        
+    }
+
+    void Wander()
+    {
+        _moveSpeed = _walkSpeed;
+
+        if (!_rotatingFlag)
+        {
+            GetWanderDirection();
+        }
+
+        transform.Rotate(0, 250 * _rotationSpeed * _rotationRate * Time.deltaTime, 0);
+        transform.Translate(Vector3.forward * _moveSpeed * Time.deltaTime);
+    }
+
+    //IEnumerator GetWanderDirection()
+    //{
+    //    _newRotation = Random.rotation;
+    //    _newRotation.x = _newRotation.z = 0f;
+    //    _rotationRate = _newRotation.y / _rotationCooldown;
+    //    _rotatingFlag = true;
+
+    //    yield return new WaitForSeconds(5f);
+    //}
+
+    void GetWanderDirection()
+    {
+        _newRotation = Random.rotation;
+        _newRotation.x = _newRotation.z = 0f;
+        _rotationRate = _newRotation.y / _rotationCooldown;
+        _rotatingFlag = true;
+    }
+
+    void Charge()
+    {
+        _moveSpeed = _runSpeed;
+
+        //_rigidbody.MovePosition(_seenPlayer.point * Time.deltaTime * _moveSpeed * 10000); *FIX CHARGING BEHAVIOR*
+        //transform.Translate(_seenPlayer.point * Time.deltaTime * _moveSpeed);
+
+        //Quaternion deltaRotation = Quaternion.Euler(new Vector 3(0, _xRotation, 0) * Time.deltaTime);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, _seenPlayer.transform.rotation, _rotationRate);
+
+
+    }
+
+    void Attack()
+    {
+
+    }
+}
